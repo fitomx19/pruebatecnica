0e41aaa0332e5d256c4036787d9c4407009f0394
fitomx19 <fitox.metallica@gmail.com>
2022-03-02 17:35:54 -0600

    prueba finalizada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
+
+import React, {useEffect,useState} from 'react';
+import axios from "axios"
+import moment from "moment";
+
 import './App.css';
+import './Apps.scss';
 
 function App() {
+  let auxiliar = []
+  const [resultados,setResultados] = useState({});
+  //api link = https://api.datos.gob.mx/v1/condiciones-atmosfericas
+  const consulta = async () =>{
+    try {
+          //consulta mediante axios al endpoint
+          const res = await axios.get("https://api.datos.gob.mx/v1/condiciones-atmosfericas").then(res=>{
+            return res.data.results
+          })
+          //ciclo para guardar el resultado en un objeto auxiliar para posteriormente guardarlo
+          //en el state
+            for(let i = 0; i< res.length; i++){
+              auxiliar.push(res[i])
+          }
+          setResultados(auxiliar)
+          //console.log(resultados)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  //funcion para realizar la busqueda de el _id del objeto en el state
+  const busqueda_id = (id)=>{
+
+      let busqueda1 = resultados.find(e => e._id === id)
+     
+      return(
+        alert(`Id: ${busqueda1._id} , nombre: ${busqueda1.name} , state: ${busqueda1.state}
+        ,probabilityofprecip: ${busqueda1.probabilityofprecip},relativehumidity: ${busqueda1.relativehumidity}, 
+        validdateutc: ${busqueda1.validdateutc}, winddirectioncardinal: ${busqueda1.winddirectioncardinal} , longitude: ${busqueda1.longitude}
+        lastreporttime: ${busqueda1.lastreporttime}, skydescriptionlong: ${busqueda1.skydescriptionlong} , stateabbr: ${busqueda1.stateabbr},
+        tempc: ${busqueda1.tempc}, latitude: ${busqueda1.latitude} , iconcode: ${busqueda1.iconcode},
+        windspeedkm: ${busqueda1.windspeedkm}, 
+        `)
+      )
+  }
+
+  //funcion de impresion de filas para la tabla donde aparece la informacion
+  const imprimir = () =>{
+    return(
+      resultados.map(function(x){
+        return(
+          <>
+           
+           <tr>
+              <td><button onClick={()=> busqueda_id(x._id)}>{x._id}</button></td>
+              <td><p>{x.cityid}</p></td>
+              <td><p><b>{x.name}</b></p></td>
+              <td><p>{x.state}</p></td>
+              <td><p>{x.probabilityofprecip}</p></td>
+              <td><p>{x.relativehumidity}</p></td>
+              <td><p>{moment(x.lastreporttime).format('YYYY-MM-DD')}</p></td>
+              <td>
+                <p>
+                {
+                  x.probabilityofprecip>60 || x.relativehumidity > 50 ?
+                  <p>Llueve</p>:<p>No llueve</p>
+                }
+                </p></td>
+            </tr>
+          
+            
+          </>
+        )
+      })
+    )
+  }
+  useEffect(()=>{
+    consulta();
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <center>
+        <div class="contenedor">
+            <table>
+                <tr>
+                  <th>_id</th>
+                  <th>cityid</th>
+                  <th>name</th>
+                  <th>state</th>
+                  <th>propability of precipi</th>
+                  <th>relative humidity</th>
+                  <th>fecha</th>
+                  <th>Llueve</th>
+                </tr>
+                {imprimir()}
+            </table>
+        </div>
+        </center>
+    </>
   );
 }
 
